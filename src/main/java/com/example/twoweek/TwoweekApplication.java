7e767fb833aaa9439c91a086cca1358bef11ac6c
Dantesiio <DavidDo29@hotmail.com>
2024-08-08 15:48:25 -0500

    endpart2

--- src/main/java/com/example/twoweek/TwoweekApplication.java
+++ src/main/java/com/example/twoweek/TwoweekApplication.java
@@ -1,23 +1,29 @@
 package com.example.twoweek;
 
 import com.example.twoweek.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.*;
 
 import java.util.Arrays;
 import java.util.logging.Logger;
 
-public class TwoweekApplication {
+@SpringBootApplication
+public class TwoweekApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
 
 	private static final Logger log=Logger.getLogger(TwoweekApplication.class.getName());
 
 	public static void main(String[] args) {
-		SpringApplication.run(TwoweekApplication.class, args);
-		ApplicationContext contex= new ClassPathXmlApplicationContext("applicationContext.xml");
-		UserRepository userRepository =contex.getBean(UserRepository.class);
-
-		log.info("the number of user is: "+ userRepository.getUsers().size());
+		SpringApplication.run(TwoweekApplication.class,args);
+	}
+	public void run(String...args){
+		log.info("Users in repository: "+ userRepository.getUsers().size());
 	}
 }
--- src/main/java/com/example/twoweek/repositories/UserRepository.java
+++ src/main/java/com/example/twoweek/repositories/UserRepository.java
@@ -1,10 +1,13 @@
 package com.example.twoweek.repositories;
 
 import com.example.twoweek.model.User;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class UserRepository {
     private List<User> users;
     public UserRepository(){
@@ -20,6 +23,7 @@ public class UserRepository {
         return users;
     }
 
+    @PostConstruct
     public void init(){
         User user = new User();
         user.setUsername("David");
